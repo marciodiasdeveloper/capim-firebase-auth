1a075a320777058789d93a00b8b31f5e6b50857c
marciodias <marciodias@pop-os.localdomain>
2024-10-29 20:32:59 -0300

    feat: create domain usecase files test blank

--- tests/domain/use-cases/user/store.spec.ts
+++ tests/domain/use-cases/user/store.spec.ts
@@ -1,8 +1,12 @@
 // import { setupUserStore, UserStoreUseCase } from '@/domain/use-cases'
+// import { v4 as uuidv4 } from 'uuid'
+// import { mock, MockProxy } from 'jest-mock-extended'
+// import { AuthEmailAlreadyInUse } from '@/application/errors'
+// import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 // describe('UserStoreUseCase', () => {
 //   let sut: UserStoreUseCase
-//   let firebase: any
+//   let sutFirebase: MockProxy<any>
 
 //   type Input = {
 //     displayName: string
@@ -19,26 +23,52 @@
 //   }
 
 //   beforeAll(() => {
-
+//     sutFirebase = mock()
+//     sutFirebase.getAuth.mockResolvedValue({})
+//     sutFirebase.getFirestore.mockResolvedValue({})
+//     sutFirebase.createUserWithEmailAndPassword.mockResolvedValue({
+//       user: {
+//         uid: uuidv4(),
+//         email: mockInput.email,
+//         getIdToken: async () => 'any_token',
+//         emailVerified: true
+//       }
+//     })
 //   })
 
 //   beforeEach(() => {
-//     sut = setupUserStore(
-//       firebase
-//     )
+//     sut = setupUserStore(sutFirebase)
 //   })
 
 //   describe('setupUserStore', () => {
 //     it('should create new user', async () => {
-//       // const customer = await sut(mockInput)
-//       // console.log(customer)
-//       // expect(customer).toEqual({ id })
+//       const customer = await sut(mockInput)
+//       console.log(customer)
+//       expect(customer).toEqual({ token: 'any_token' })
+//     })
+
+//     it('deve lançar AuthEmailAlreadyInUse quando o email já estiver em uso', async () => {
+//       class AuthError extends Error {
+//         code: string
+//         constructor (message: string, code: string) {
+//           super(message)
+//           this.code = code
+//         }
+//       }
+
+//       const error = new AuthError('auth/email-already-in-use', 'auth/email-already-in-use')
+//       sutFirebase.createUserWithEmailAndPassword.mockRejectedValue(error)
+
+//       const firebase = sutFirebase
+//       const userStoreUseCase = setupUserStore({ firebase })
+
+//       await expect(userStoreUseCase(mockInput)).rejects.toThrow(AuthEmailAlreadyInUse)
+//       expect(createUserWithEmailAndPassword).toHaveBeenCalledWith(firebase, mockInput.email, mockInput.password)
 //     })
 //   })
 // })
-
-describe('UserStoreUseCase', () => {
-  it('should create new user', async () => {
+describe(' TaskDeleteUseCase', () => {
+  it('should delete a task', async () => {
 
   })
 })
