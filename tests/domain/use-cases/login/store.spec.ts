b89da1635786d4b48f5f920c9b5400a49be0c208
marcio dias <marcio@marciodias.me>
2024-10-31 10:59:44 -0300

    feat: test firebase auth

--- tests/domain/use-cases/login/store.spec.ts
+++ tests/domain/use-cases/login/store.spec.ts
@@ -1,6 +1,6 @@
-// import { setupLoginStore, LoginStoreUseCase } from '@/domain/use-cases'
-// import { mock, MockProxy } from 'jest-mock-extended'
-// import { FirebaseApp } from 'firebase/app'
+import { setupLoginStore, LoginStoreUseCase } from '@/domain/use-cases'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { FirebaseApp } from 'firebase/app'
 
 // jest.mock('firebase/app', () => {
 //   const userCredentialMock = {
@@ -24,49 +24,51 @@
 //   }
 // })
 
-// jest.mock('firebase/auth')
-
-jest.mock('firebase/auth', () => {
-  return {
-    auth: jest.fn().mockReturnThis(),
-    signInWithEmailAndPassword: jest.fn(async () => {
-      return new Promise((resolve, reject) => {
-        resolve(null)
-      })
-    }),
-    getAuth: jest.fn()
-  }
-})
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn().mockReturnValue({
+    // validateUser: jest.fn().mockReturnValue({
+    //   email: 'myEmail@gmail.com',
+    //   name: 'myName',
+    //   sub: 'e87255a8-92f5-4de5-b295-6dc74c0a3ccb',
+    //   roles: ['user', 'owner', 'admin'],
+    //   lang: 'pt-BR',
+    //   tenantId: '4cd086d1-cfc2-491a-8099-75c6d27dfe62'
+    // })
+  }),
+  signInWithEmailAndPassword: jest.fn().mockResolvedValue({
+    user: {
+      getIdToken: jest.fn().mockResolvedValue('any_token'),
+      getIdTokenResult: jest.fn().mockResolvedValue
+    }
+  })
+}))
 
 describe('LoginStoreUseCase', () => {
-  // let sut: LoginStoreUseCase
-  // let sutFirebase: MockProxy<FirebaseApp>
+  let sut: LoginStoreUseCase
+  let sutFirebase: MockProxy<FirebaseApp>
 
-  // type Input = {
-  //   email: string
-  //   password: string
-  // }
+  type Input = {
+    email: string
+    password: string
+  }
 
-  // const mockInput: Input = {
-  //   email: 'foo@bar.com',
-  //   password: '123456'
-  // }
+  const mockInput: Input = {
+    email: 'foo@bar.com',
+    password: '123456'
+  }
 
-  // beforeAll(() => {
-  //   sutFirebase = mock()
-  //   sutFirebase.getAuth.mockResolvedValue({})
-  //   sutFirebase.signInWithEmailAndPassword.mockResolvedValue({})
-  // })
+  beforeAll(() => {
+    sutFirebase = mock()
+  })
 
-  // beforeEach(() => {
-  //   sut = setupLoginStore(sutFirebase)
-  // })
+  beforeEach(() => {
+    sut = setupLoginStore(sutFirebase)
+  })
 
   describe('setupLoginStore', () => {
-    it('should create new user', async () => {
-      // const customer = await sut(mockInput)
-      // console.log(customer)
-      // expect(customer).toEqual({ token: 'any_token' })
+    it('should login user', async () => {
+      const data = await sut(mockInput)
+      expect(data).toEqual({ token: 'any_token' })
     })
   })
 })
