04f343a45c8dcc98a7d0b7a04ffb824120b797b4
marcio dias <marcio@marciodias.me>
2024-10-31 14:08:29 -0300

    feat: add forgot password controller added

--- tests/application/controllers/ping/ping.spec.ts
+++ tests/application/controllers/ping/ping.spec.ts
@@ -1,19 +1,13 @@
-import { Controller, UserStoreController } from '@/application/controllers'
+import { Controller, PingController } from '@/application/controllers'
 import { ServerError } from '@/application/errors'
-import { RequiredString } from '@/application/validation'
-import { AuthorizationTokenError, AuthorizationDomainNotValidError } from '@/domain/entities'
-
 import { v4 as uuidv4 } from 'uuid'
 
-describe('UserStoreController', () => {
-  let sut: UserStoreController
+describe('PingController', () => {
+  let sut: PingController
   let sutAuthorizationToken: jest.Mock
 
   const mockHttpRequestInput = {
-    displayName: 'Foo Bar',
-    phoneNumber: '+5537999999999',
-    email: 'foo@bar.com',
-    password: '123456'
+    email: 'foo@bar.com'
   }
 
   beforeAll(() => {
@@ -22,65 +16,18 @@ describe('UserStoreController', () => {
   })
 
   beforeEach(() => {
-    sut = new UserStoreController(sutAuthorizationToken)
+    sut = new PingController(sutAuthorizationToken)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
-  })
-  it('should call Category with correct input', async () => {
-    await sut.handle(mockHttpRequestInput)
-    expect(sutAuthorizationToken).toHaveBeenCalledWith(mockHttpRequestInput)
-    expect(sutAuthorizationToken).toHaveBeenCalledTimes(1)
-  })
-
-  it('should return 400 if AuthorizationTokenError fails', async () => {
-    sutAuthorizationToken = jest.fn()
-    sutAuthorizationToken.mockRejectedValueOnce(new AuthorizationTokenError())
-    sut = new UserStoreController(sutAuthorizationToken)
-
-    const httpResponse = await sut.handle(mockHttpRequestInput)
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new AuthorizationTokenError()
-    })
-  })
-
-  it('should return 400 if AuthorizationDomainNotValidError fails', async () => {
-    sutAuthorizationToken = jest.fn()
-    sutAuthorizationToken.mockRejectedValueOnce(new AuthorizationDomainNotValidError())
-    sut = new UserStoreController(sutAuthorizationToken)
-
-    const httpResponse = await sut.handle(mockHttpRequestInput)
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new AuthorizationDomainNotValidError()
-    })
-  })
-
-  it('should build Validators correctly on save', async () => {
-    const validators = sut.buildValidators({
-      displayName: '',
-      phoneNumber: '',
-      email: '',
-      password: ''
-    })
-
-    expect(validators).toEqual([
-      new RequiredString('', 'displayName'),
-      new RequiredString('', 'phoneNumber'),
-      new RequiredString('', 'email'),
-      new RequiredString('', 'password')
-    ])
   })
 
   it('should return 500 if fails new Error', async () => {
     sutAuthorizationToken = jest.fn()
     sutAuthorizationToken.mockRejectedValueOnce(new Error())
 
-    sut = new UserStoreController(sutAuthorizationToken)
+    sut = new PingController(sutAuthorizationToken)
 
     const httpResponse = await sut.handle(mockHttpRequestInput)
 
