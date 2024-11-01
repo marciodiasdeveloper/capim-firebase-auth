bcb2269662f292984a8c19b692e2df2955539522
marcio dias <marcio@marciodias.me>
2024-11-01 16:20:52 -0300

    feat: add new reset password confirm test use case

--- tests/domain/entities/page-filter.spec.ts
+++ tests/domain/entities/page-filter.spec.ts
@@ -29,4 +29,53 @@ describe('PageFilter', () => {
     }).validate()
     expect(getUrl).toEqual({ page: 1, rowsPerPage: 10, search: 'search' })
   })
+
+  describe('PageFilter', () => {
+    it('should validate empty page', () => {
+      const getUrl = new PageFilter({}).validate()
+      expect(getUrl).toEqual({ page: 1, rowsPerPage: 10, search: undefined })
+    })
+    it('should validate page', () => {
+      const getUrl = new PageFilter({
+        page: 2
+      }).validate()
+      expect(getUrl).toEqual({ page: 2, rowsPerPage: 10, search: undefined })
+    })
+    it('should validate page negative', () => {
+      const getUrl = new PageFilter({
+        page: -1
+      }).validate()
+      expect(getUrl).toEqual({ page: 1, rowsPerPage: 10, search: undefined })
+    })
+    it('should validate rowsPerPage', () => {
+      const getUrl = new PageFilter({
+        rowsPerPage: 50
+      }).validate()
+      expect(getUrl).toEqual({ page: 1, rowsPerPage: 50, search: undefined })
+    })
+    it('should validate search', () => {
+      const getUrl = new PageFilter({
+        search: 'search'
+      }).validate()
+      expect(getUrl).toEqual({ page: 1, rowsPerPage: 10, search: 'search' })
+    })
+    it('should validate rowsPerPage as NaN', () => {
+      const getUrl = new PageFilter({
+        rowsPerPage: NaN
+      }).validate()
+      expect(getUrl).toEqual({ page: 1, rowsPerPage: 10, search: undefined })
+    })
+    it('should validate rowsPerPage as negative', () => {
+      const getUrl = new PageFilter({
+        rowsPerPage: -5
+      }).validate()
+      expect(getUrl).toEqual({ page: 1, rowsPerPage: 10, search: undefined })
+    })
+    it('should validate page as NaN', () => {
+      const getUrl = new PageFilter({
+        page: NaN
+      }).validate()
+      expect(getUrl).toEqual({ page: 1, rowsPerPage: 10, search: undefined })
+    })
+  })
 })
