d37a09790b13cc8406a03e44163924b2c4e0e5af
marcio dias <marcio@marciodias.me>
2024-11-01 15:55:54 -0300

    feat: add new reset password confirm test use case

--- tests/domain/use-cases/reset-password-confirm/store.spec.ts
+++ tests/domain/use-cases/reset-password-confirm/store.spec.ts
@@ -1,5 +1,38 @@
-describe(' TaskDeleteUseCase', () => {
-  it('should delete a task', async () => {
+import { setupResetPasswordConfimStore, ResetPasswordConfirmStoreUseCase } from '@/domain/use-cases'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { FirebaseApp } from 'firebase/app'
 
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn().mockReturnValue({}),
+  confirmPasswordReset: jest.fn().mockResolvedValue({})
+}))
+
+describe('ResetPasswordConfirmStoreUseCase', () => {
+  let sut: ResetPasswordConfirmStoreUseCase
+  let sutFirebase: MockProxy<FirebaseApp>
+
+  type Input = {
+    oobCode: string
+    newPassword: string
+  }
+
+  const mockInput: Input = {
+    oobCode: 'any_oobCode',
+    newPassword: 'any_new_password'
+  }
+
+  beforeAll(() => {
+    sutFirebase = mock()
+  })
+
+  beforeEach(() => {
+    sut = setupResetPasswordConfimStore(sutFirebase)
+  })
+
+  describe('setupResetPasswordConfimStore', () => {
+    it('should request forgot password', async () => {
+      const data = await sut(mockInput)
+      expect(data).toEqual({})
+    })
   })
 })
